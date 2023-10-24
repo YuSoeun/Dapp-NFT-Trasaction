33e126f780d6b43dfdd5676a8937d4346447756b
YuSoeun <doplinblue@handong.ac.kr>
2023-10-24 14:05:08 +0900

    set web3 related ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Main from "./routes/main";
+import { mintAnimalTokenContract } from "./";
 
 const App: FC = () => {
-  return <div>web3-boilerplate</div>;
+  const [account, setAccount] = useState<string>("");
+
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAccount(accounts[0]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAccount();
+  }, [account]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main account={account}/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/routes/main.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useState } from 'react';
+import { Box, Text, Flex, Button } from '@chakra-ui/react'
+
+interface MainProps {
+  account: string;
+}
+
+const Main: FC<MainProps> = ({ account }) => {
+  const [newAnimalCard, setNewAnimalCard] = useState<string>();
+
+  const onClickMint = async () => {
+    try {
+      if (!account) return;
+
+      const response = await mintAnimalTokenContract.methods.mintAnimalToken().send();
+        .mintAnimalToken()
+        .send({from: account});
+
+        console.log(response);
+    } catch (error) {
+      
+    }
+  };
+
+  return (
+    <Flex w="full" h="100vh" justifyContent="center" alignItems="center" direction="column">
+      <Box>
+        {newAnimalCard ? (
+          <div>AnimalCard</div>
+        ) : (
+          <Text>Let's mint Animal Card!!!</Text>
+        )}
+      </Box>
+      <Button mt={4} size="sm" colorScheme='blue' onClick={onClickMint}>Mint</Button>
+    </Flex>
+  )
+};
+
+export default Main;
